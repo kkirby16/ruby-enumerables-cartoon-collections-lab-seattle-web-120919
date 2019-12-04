31c137ef8d8adc02ff5d0e4d513c7161bfe18101
Kevin Kirby <kkirby16@my.whitworth.edu>
2019-12-04 23:44:55 +0000

    Automatically backed up by Learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -18,7 +18,7 @@ end
 
 def find_the_cheese(array) #??ask question to fully understand #this one??
    cheese_types = ["cheddar", "gouda", "camembert"]
-   array.find { |x| cheese_types.include?(x) }
+   array.find { |x| cheese_types.include?(x) } #can use binding.pry within this line to see what x returns and x returns the first element of the array, then the second, and then the 3rd because .find is an iteration since it checks all the elements of the array for the first that satisfies.
    
 end  
 
